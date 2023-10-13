a3d1d7a641ed13c8625b744aea99cc5b975aaf82
luketto <115785443+luckybros@users.noreply.github.com>
2023-10-13 16:31:40 +0200

    problema 217

---
+++ Problem217.java
@@ -0,0 +1,19 @@
+package LeetCodeProblems;
+import java.util.Arrays;
+
+class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 1; i++)
+            if(nums[i] == nums[i+1])
+                return true;
+
+        return false;
+    }
+
+    public static void main(String[] arg) {
+        int nums[] = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
