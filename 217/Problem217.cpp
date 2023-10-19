1f74260c5f37626b10c6d9f8e41e38b03c12fcd4
luca <luca.scolletta@outlook.it>
2023-10-19 09:54:16 +0200

    Aggiunta soluzione in tempo lineare per il problema 217

--- 217/Problem217.cpp
+++ 217/Problem217.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
@@ -12,12 +13,22 @@ public:
                 return true;
         return false;
     }
+
+    bool containsDuplicateLinear(vector<int>& nums) {
+        unordered_set<int> seen;
+        for(int num: nums) {
+            if(seen.count(num) > 0)
+                return true;
+            seen.insert(num);
+        }
+        return false;
+    }
 };
 
 int main() {
     Solution solution;
     int x[] = {1,2,3,1};
     vector<int> nums(begin(x), end(x));
-    cout << solution.containsDuplicate(nums);
+    cout << solution.containsDuplicateLinear(nums);
     return 0;
 }
