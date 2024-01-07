9cdf2f42ebf3039af2aba5e1a4b29c32f6f445bd
luca <luca.scolletta@outlook.it>
2024-01-07 17:30:54 +0100

    modificato targetsum

--- 999999/TargetSum.java
+++ 999999/TargetSum.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 class TargetSum {
     public List<Integer> findTargetSum(int[] nums, int target) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         binarySearch(nums, 0, nums.length-1, target, result);
 
@@ -27,12 +28,11 @@ class TargetSum {
             binarySearch(nums, right+1, left, target, result);
         }
         else if(sum > target) binarySearch(nums, right, left-1, target, result);
-        else if(sum < target) binarySearch(nums, right, left-1, target, result);
+        else if(sum < target) binarySearch(nums, right+1, left, target, result);
     }
     
     public static void main(String[] arg) {
         int nums[] = {10, 2, 6, 8, 4};
-        Arrays.sort(nums);
         TargetSum solution = new TargetSum();
         for (Integer i : solution.findTargetSum(nums, 10)) 
             System.out.println(i);
